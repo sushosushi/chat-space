3e8e1afab2ee63b00f24fed866cc0caf6db2abc7
藤田澄智 <sumitoshi@fujitakiyoshisatoshinoMacBook-puro.local>
2018-03-18 16:34:40 +0900

    グループ機能
    機能作成、サイドバー表示

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,9 +4,17 @@ class GroupsController < ApplicationController
   end
 
   def new
+    @group = Group.new
+    @group.users << current_user
   end
 
   def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: 'グループを作成しました。'
+    else
+      render :new
+    end
   end
 
   def edit
@@ -15,5 +23,9 @@ class GroupsController < ApplicationController
   def update
   end
 
+  private
+  def group_params
+    params.require(:group).permit(:name, user_ids: [] )
+  end
 
 end
