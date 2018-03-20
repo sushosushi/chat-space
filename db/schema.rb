7c0eefe336dc4f18a58e693da714645fc715ba34
藤田澄智 <sumitoshi@fujitakiyoshisatoshinoMacBook-puro.local>
2018-03-20 20:46:46 +0900

    message_transmission_function

--- db/schema.rb
+++ db/schema.rb
@@ -28,8 +28,14 @@ ActiveRecord::Schema.define(version: 20180318104519) do
   end
 
   create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "content"
+    t.string   "image"
+    t.integer  "group_id"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["group_id"], name: "index_messages_on_group_id", using: :btree
+    t.index ["user_id"], name: "index_messages_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -52,4 +58,6 @@ ActiveRecord::Schema.define(version: 20180318104519) do
 
   add_foreign_key "group_users", "groups"
   add_foreign_key "group_users", "users"
+  add_foreign_key "messages", "groups"
+  add_foreign_key "messages", "users"
 end
