03173dd80c6719b198696eacde82d4ded81de773
藤田澄智 <sumitoshi@fujitakiyoshisatoshinoMacBook-puro.local>
2018-04-06 21:17:29 +0900

    incremental research

---
+++ app/assets/javascripts/group.js
@@ -0,0 +1,65 @@
+$(function() {
+
+  var search_list = $("#user-search-result");
+
+  function appendUser(user) {
+    var html = `
+      <div class="chat-group-user clearfix">
+        <p class="chat-group-user__name">${user.user_name}</p>
+        <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${user.user_id} data-user-name=${user.user_name}>追加</a>
+      </div>`
+    search_list.append(html);
+  }
+
+  function appendNoUser(user) {
+    var html = `
+      <div class="chat-group-user clearfix">
+        <p class="chat-group-user__name">${user}</p>
+      </div>`
+    search_list.append(html);
+  }
+  function appendHTML(user_id, user_name) {
+    var html =`
+      <div class='chat-group-user clearfix js-chat-member' id='chat-group-user-${user_id}'>
+        <input name='group[user_ids][]' type='hidden' value='${user_id}'>
+        <p class='chat-group-user__name'>${user_name}</p>
+        <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+      </div>`
+    return html;
+  }
+
+  $(document).on("click", ".user-search-add", function(){
+    var user_id = $(this).attr('data-user-id');
+    var user_name = $(this).attr('data-user-name');
+    $(this).parent().remove();
+    $('#chat-group-user').append(appendHTML(user_id, user_name));
+  })
+
+  $(document).on("click", ".user-search-remove", function(){
+    $(this).parent().remove();
+  })
+
+  $("#user-search-field").on("keyup", function() {
+    var input = $("#user-search-field").val();
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      data: { keyword: input },
+      dataType: 'json'
+    })
+
+    .done(function(users) {
+      $("#user-search-result").empty();
+      if(users.length !== 0) {
+        users.forEach(function(user) {
+          appendUser(user);
+        });
+      } else {
+        appendNoUser("一致するユーザはいません。");
+      }
+    })
+    .fail(function() {
+      alert('ユーザ検索に失敗しました。');
+    })
+  });
+});
--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -21,7 +21,7 @@ class GroupsController < ApplicationController
 
   def update
     if @group.update(group_params)
-      redirect_to groups_path(@group), notice: 'グループを編集しました。'
+      redirect_to group_messages_path(@group), notice: 'グループを編集しました。'
     else
       render :edit
     end
@@ -29,7 +29,7 @@ class GroupsController < ApplicationController
 
   private
   def group_params
-    params.require(:group).permit(:name, user_ids: [] )
+    params.require(:group).permit(:name, user_ids: [])
   end
 
   def set_group
