bcbff64d1dd53a8a6253fee59771b9cba785371d
藤田澄智 <sumitoshi@fujitakiyoshisatoshinoMacBook-puro.local>
2018-04-15 18:40:21 +0900

    自動更新

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -3,26 +3,50 @@ $(function() {
   function buildHTML(message){
     message.image == null ? image_html = `` : image_html = `<img class="chat-message__body-message-image" src="${message.image}">`
     var html = `
-    <div class=chat-message>
-      <div class=chat-message__body-member-list>
-        <div class=chat-message__body-member>
+    <div class="chat-message" data-message-id="${message.id}">
+      <div class="chat-message__body-member-list">
+        <div class="chat-message__body-member">
           ${message.user_name}
         </div>
-        <div class=chat-message__body-date>
+        <div class="chat-message__body-date">
           ${message.created_at}
         </div>
-        <div class=chat-message__body-message>
-            <div class=chat-message__body-message-content>
-              ${message.content}
-            </div>
-            ${image_html}
-        </div>
+      </div>
+      <div class="chat-message__body-message">
+          <div class="chat-message__body-message-content">
+            ${message.content}
+          </div>
+          ${image_html}
       </div>
     </div>`
     return html;
   }
 
-
+  var interval = setInterval(function(){
+    var last_id = $('.chat-message:last').data('message-id');
+    if (typeof last_id === 'undefined'){ return false }
+    var url = window.location.href;
+    if (url.match(/\/groups\/\d+\/messages/)){
+    $.ajax({
+      type: "GET",
+      url: url,
+      data: { id: last_id },
+      dataType: 'json'
+    })
+    .done(function(data){
+      if (data.length !== 0){
+        data.forEach(function(message){
+          $(".chat-messages").append(buildHTML(message));
+        });
+      }
+      $('.chat-messages').animate({scrollTop: $('.chat-messages')[0].scrollHeight}, 'fast');
+    })
+    .fail(function(data) {
+      alert('error');
+    });
+  } else {
+    clearInterval(interval);
+  }}, 5000);
 
   $('#new_message').on('submit', function(e) {
     e.preventDefault();
