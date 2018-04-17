f715d43c73527e300a705291632270cdb7e08fdb
藤田澄智 <sumitoshi@fujitakiyoshisatoshinoMacBook-puro.local>
2018-04-17 20:04:23 +0900

    A

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.10.2"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "chat-space"
+set :repo_url, "git@github.com:sushosushi/chat-space.git"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
