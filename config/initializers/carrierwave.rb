456aa77fe19e95906a556b49b78f669a59d502d5
藤田澄智 <>
2018-06-16 21:01:01 +0900

    modify IAM

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,6 +12,6 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1'
   }
 
-  config.fog_directory  = 'assdff123'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/assdff123'
+  config.fog_directory  = 'chat-upload123'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chat-upload123'
 end
